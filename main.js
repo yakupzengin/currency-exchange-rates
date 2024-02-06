9c485596f1bdb7fe5568790919a7984f7ddb24db
Yakup Zengin <clafedersin@gmail.com>
2024-02-07 00:11:27 +0300

    added functions and fields

---
+++ main.js
@@ -0,0 +1,23 @@
+// get elements
+
+const amountInput = document.querySelector("#amount");
+const firstOption = document.querySelector("#firstCurrencyOption")
+const secondOption = document.querySelector("#secondCurrencyOption")
+const resultInput = document.querySelector("#result")
+
+const currency = new Currency() 
+
+runEventListener();
+
+function runEventListener(){
+    amountInput.addEventListener("input",exchange)
+}
+
+function exchange(){
+    const amount = Number(amountInput.value.trim());
+    const firstOptionValue = firstOption.options[firstOption.selectedIndex].textContent;
+    const secondOptionValue = secondOption.options[secondOption.selectedIndex].textContent;
+    console.log("firstOptionValue" + firstOptionValue + "secondOptionValue" + secondOptionValue)
+
+    currency.exchange(amount,firstOptionValue,secondOptionValue)
+}
